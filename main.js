7af9922e1bc1d8e3524fec49a10550b7dc47cd27
OmarMFattouh <162017850+OmarMFattouh@users.noreply.github.com>
2024-06-07 03:08:10 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,25 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+let section=document.querySelectorAll('main section')
+let navLink=document.querySelectorAll('header nav ul li a')
+window.onscroll=()=>{
+    section.forEach(sec=>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id')
+        if(top>offset && top<offset + height){
+            navLink.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav ul li a[href*='+id+']').classList.add('active')
+
+            })
+        }
+    })
+}
+
 //  function changeColor(){
 //     var elmnt = document.lu;
 //     elmnt.style.backgroundColor = "white";
